ca7e845405562fe6edf8b4b7c1f60c1109451830
brandonfwl <brandonfwl@gmail.com>
2013-11-21 13:02:14 -0800

    change to functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -8,9 +8,31 @@ to avoid linting crap
 """
 
 from selenium import webdriver
+import unittest
 
 
-BROWSER = webdriver.Firefox()
-BROWSER.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in BROWSER.title
+    """.
+
+        New visitor test case
+        For functional testing
+
+    """
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
